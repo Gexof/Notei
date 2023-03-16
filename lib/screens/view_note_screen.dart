2e04b8f8d9c5b164856778d58e60a2e1d07b1eb1
Mohamed Ashraf <gexof01@gmail.com>
2023-03-16 19:20:25 +0200

    don't know anything

--- lib/screens/view_note_screen.dart
+++ lib/screens/view_note_screen.dart
@@ -1,3 +1,52 @@
+// import 'package:flutter/material.dart';
+// import 'package:gex_note/providers/note_provider.dart';
+// import 'package:provider/provider.dart';
+
+// import '../constants/colors.dart';
+// import '../models/note_model.dart';
+
+// class ViewNoteScreen extends StatelessWidget {
+//   const ViewNoteScreen({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     NoteProvider notesProvider = Provider.of<NoteProvider>(context);
+
+//     return SafeArea(
+//       child: Scaffold(
+//         backgroundColor: backgroundColor,
+//         body: Padding(
+//           padding: const EdgeInsets.all(16.0),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               Text(
+//                 '${notesProvider.notes[1].title}',
+//                 style: const TextStyle(
+//                   fontSize: 18,
+//                   fontWeight: FontWeight.bold,
+//                 ),
+//               ),
+//               const Divider(color: Colors.black38),
+//               const Expanded(
+//                 child: SizedBox(
+//                   height: double.infinity,
+//                   child: Text(
+//                     'Gex first note you can ever fucking see it m hahahahhahahahahahhahaha hhahahahhahahah hahahahhahahahah ahhahahahaha you ou ouou ',
+//                     style: TextStyle(
+//                       fontSize: 16,
+//                     ),
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 import 'package:gex_note/providers/note_provider.dart';
 import 'package:provider/provider.dart';
@@ -21,14 +70,14 @@ class ViewNoteScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                '{noteModel.title}',
-                style: TextStyle(
+                '${notesProvider.notes[1].title}',
+                style: const TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              Divider(color: Colors.black38),
-              Expanded(
+              const Divider(color: Colors.black38),
+              const Expanded(
                 child: SizedBox(
                   height: double.infinity,
                   child: Text(
